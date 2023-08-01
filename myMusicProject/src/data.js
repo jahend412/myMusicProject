84c082b47566f261c52e458f3aba3debf3192a1b
Josh Endemann <jahend44@gmail.com>
2023-08-01 05:20:26 -0500

    Create data.js

---
+++ myMusicProject/src/data.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        id: 1,
+        bandName: "Radiohead",
+        bandImage: 'radiohead.png',
+        description: "Radiohead, British rock group that was arguably the most accomplished art-rock band of the early 21st century. This revered quintet made some of the most majestic—if most angst-saturated—music of the postmodern era.",
+        bandAlbums: '',
+        bandMembers: '',
+        genre: '',
+        location: "Oxfordshire, England",
+        openSpots: 0,
+    },
+]
