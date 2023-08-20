ad334469ccad85476063003ba87371bef0cb726f
Josh Endemann <jahend44@gmail.com>
2023-08-20 12:32:26 -0500

    Add  to audiodata

--- react-music-player/src/audioData.js
+++ react-music-player/src/audioData.js
@@ -2,5 +2,25 @@ export const audioData = [
     {
         title: "Wish You Were Here",
         artist: "Pink Floyd"
-    }
+    },
+    {
+        title: "Stairway to Heaven",
+        artist: "Led Zeppelin"
+    },
+    {
+        title: "Bohemian Rhapsody",
+        artist: "Queen"
+    },
+    {
+        title: "Comfortably Numb",
+        artist: "Pink Floyd"
+    },
+    {
+        title: "Hey Jude",
+        artist: "The Beatles"
+    },
+    {
+        title: "Hotel California",
+        artist: "Eagles"
+    },
 ];
